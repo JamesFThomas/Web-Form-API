a6941a601fa68d35c92ac22d0c674e3aef384edd
James F. Thomas <jthomas@rsimail.com>
2025-03-08 10:34:45 -0600

    removed commented code from FormsRespository class and interface

--- Web Form API/Repository/IFormsRespository.cs
+++ Web Form API/Repository/IFormsRespository.cs
@@ -8,15 +8,6 @@ namespace Web_Form_API.Repository
 {
     public interface IFormsRespository : IGenericRepository<FormBase>
     {
-        //IEnumerable<FormBase> GetAllForms();
-
-        //FormBase GetForm(int id);
-
-        //void AddForm(FormBase form);
-
-        //void UpdateForm(FormBase form);
-
-        //void DeleteForm(int id);
 
         // route specifc for the Forms data not used by teh generic repository class
         IEnumerable<FormBase> GetCompletedForms();
